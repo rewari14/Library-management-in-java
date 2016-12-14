e63f5bfb7a35efeead80d75067e9a7c73cb5dc10
rewari14 <shubham.rewari14@gmail.com>
2016-12-14 12:56:44 -0800

    Update main.java

--- main.java
+++ main.java
@@ -8,18 +8,18 @@ public class Main {
     public static void main(String[] args) {
     ArrayList<String> l = new ArrayList<String>();    
         
-    Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "shubham loves this book");
-    Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "shubham hates this book");
-    Book b1 = new Book("Hello Brother", 1814, "shubham hates this book");
-    Book b2 = new Book("Kabaali", 2016, "shubham loves this book");
+    Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "Shubham loves this book");
+    Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "Shubham hates this book");
+    Book helloBrother = new Book("Hello Brother", 1814, "Shubham hates this book");
+    Book kabaali = new Book("Kabaali", 2016, "Shubham loves this book");
 
     
     BookLibrary lib = new BookLibrary();
    
     lib.addBook(prideAndPrejudice);
     lib.addBook(senseAndSensibility);
-    lib.addBook(b1);
-    lib.addBook(b2);
+    lib.addBook(helloBrother);
+    lib.addBook(kabaali);
    
     l = lib.getBookList();
     
